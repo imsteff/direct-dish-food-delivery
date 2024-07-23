690bfce4cfe9358974a1aef7a2405df43f2dcfb4
Stephanie Encomienda <stphnencomienda30@gmail.com>
2024-07-23 23:28:18 +0800

    Updated the Home page
    Next update is responsiveness

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,29 +1,29 @@
-import React, { useContext } from 'react'
-import PropTypes from 'prop-types'
-import './FoodDisplay.css'
-import { StoreContext } from '../../context/StoreContext'
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import './FoodDisplay.css';
+import { StoreContext } from '../../context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
 
 const FoodDisplay = ({ category }) => {
-  const { food_list } = useContext(StoreContext)
+  const { food_list } = useContext(StoreContext);
 
   return (
     <div className='food-display' id='food-display'>
       <h2>Top dishes near you</h2>
-      {/* Example usage of category and food_list */}
-      {food_list && food_list
-        .filter(item => item.category === category)
-        .map((item, index) => (
-          <div key={index} className='food-item'>
-            <img src={item.image} alt={item.name} />
-            <p>{item.name}</p>
-          </div>
-        ))}
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          if (category==="All" || category===item.category) {
+            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+          }
+          
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
 FoodDisplay.propTypes = {
   category: PropTypes.string.isRequired,
-}
+};
 
-export default FoodDisplay
+export default FoodDisplay;
